704bb87460659ca64852322b2e3724f496bc1e99
jazreel <jazreel.kwek@gmail.com>
2021-03-10 17:13:48 +0800

    models and dataloader update

--- model_binary.py
+++ model_binary.py
@@ -10,17 +10,52 @@ from torchvision import transforms
 class Model_binary_1(nn.Module):
     def __init__(self):
         super(Model_binary_1, self).__init__()
-        # Conv2D: 1 input channel, 4 output channels, 3 by 3 kernel, stride of 1.
-        self.conv1 = nn.Conv2d(1, 4, 3, 1)
+        # input img = 150x150
+        # Conv2D: 1 input channel, 16 output channels, 7 kernel, stride of 1. -> 144x144x16
+        self.conv1 = nn.Conv2d(1, 16, 7, 1)
+
         # Conv2D: 4 input channel, 8 output channels, 5 by 5 kernel, stride of 2.
-        self.conv2 = nn.Conv2d(4, 8, 5, 2)
-        self.fc1 = nn.Linear(41472, 2)
+        # self.conv2 = nn.Conv2d(8, 16, 5, 1)
+
+        # maxpooling -> 48x48x16
+        self.pool = nn.MaxPool2d(3, 3)
+
+        self.conv3 = nn.Conv2d(16, 16, 3, 1, 1)
+        self.conv4 = nn.Conv2d(16, 16, 3, 1, 1)
+
+        self.conv5 = nn.Conv2d(16, 16, 3, 1, 1)
+        self.conv6 = nn.Conv2d(16, 16, 3, 1, 1)
+
+        self.conv7 = nn.Conv2d(16, 16, 3, 1, 1)
+        self.conv8 = nn.Conv2d(16, 16, 3, 1, 1)
+
+        self.fc1 = nn.Linear(36864, 1000)
+        self.fc2 = nn.Linear(1000, 100)
+        self.fc3 = nn.Linear(100, 2)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.conv2(x)
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+
+        residual = x
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x += residual
+
+        residual = x
+        x = F.relu(self.conv5(x))
+        x = F.relu(self.conv6(x))
+        x += residual
+
+        residual = x
+        x = F.relu(self.conv7(x))
+        x = F.relu(self.conv8(x))
+        x += residual
+
         x = torch.flatten(x, 1)
         x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
         output = F.softmax(x, dim=1)
         return output
 
@@ -30,7 +65,7 @@ print(model)
 
 ld_train = Lung_Dataset("train", "binary")
 
-batch_size = 8
+batch_size = 512
 
 
 train_loader = DataLoader(ld_train, batch_size=batch_size, shuffle=True)
@@ -56,12 +91,13 @@ for epoch in range(50):  # loop over the dataset multiple times
         optimizer.step()
 
         # print statistics
-        running_loss += loss.item()
+        # running_loss += loss.item()
+        print(loss.item())
         # print(loss.item())
-        if i % 100 == 99:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 100))
-            running_loss = 0.0
+        # if i % 10 == 9:    # print every 2000 mini-batches
+        #     print('[%d, %5d] loss: %.3f' %
+        #           (epoch + 1, i + 1, running_loss / 9))
+        #     running_loss = 0.0
 
     # print(f'Epoch: {i} Loss: {running_loss}')
 
--- model_three_class.py
+++ model_three_class.py
@@ -13,38 +13,45 @@ class ModelThreeClass(nn.Module):
         super(ModelThreeClass, self).__init__()
 
         inp_img_size = 150
-        kernel_size = 3
+        kernel_size = 7
         stride = 1
         padding = 0
-        conv1_output_channels = 4
-        conv2_output_channels = 8
+        conv1_output_channels = 16
+        conv2_output_channels = 16
         conv3_output_channels = 16
 
         # conv1_output_size = (inp_img_size-kernel_size+2*padding)/stride + 1;
         # input img: 150x150
         self.conv1 = nn.Conv2d(1, conv1_output_channels, kernel_size, stride)
-        # input: 148x148x4
+        # input: 144x144x4
         self.conv2 = nn.Conv2d(conv1_output_channels,
-                               conv2_output_channels, kernel_size, stride)
-        # input: 146x146x8
+                               conv2_output_channels, kernel_size, stride, padding=3)
+        # input: 144x144x8
         self.pool = nn.MaxPool2d(2, 2)
-        # input: 73x73x8
+        # input: 72x72x8
         self.conv3 = nn.Conv2d(conv2_output_channels,
-                               conv3_output_channels, kernel_size, stride)
-        # input: 71x71x16
-        self.fc1 = nn.Linear(80656, 1024)
+                               conv3_output_channels, kernel_size, stride, padding=3)
+        # input: 72x72x16
+        self.fc1 = nn.Linear(20736, 1024)
         self.fc2 = nn.Linear(1024, 120)
         self.fc3 = nn.Linear(120, 3)
         # self.out = nn.Softmax(3)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.conv2(x)
+        x = F.relu(self.conv1(x))
+
+        residual = x
+        x = F.relu(self.conv2(x))
+        x += residual
+
         x = self.pool(x)
-        x = self.conv3(x)
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = self.fc2(x)
+        x = F.relu(self.conv3(x))
+
+        x = self.pool(x)
+        x = torch.flatten(x, 1)        
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        
         x = self.fc3(x)
         return F.softmax(x, dim=1)
         # output = F.log_softmax(x, dim=1)
@@ -52,36 +59,69 @@ class ModelThreeClass(nn.Module):
         # return output
 
 
-# with open('./dataset/test/infected/covid/0.jpg', 'rb') as f:
-#     # Convert to Numpy array and normalize pixel values by dividing by 255.
-#     im = np.asarray(Image.open(f))/255
-#     im = transforms.functional.to_tensor(np.array(im)).float()
-#     im = im.unsqueeze(0)
-#     print(im.shape)
-#     target = torch.Tensor([1]).long()
-#     print(target.shape)
-#     # target = transforms.functional.to_tensor(np.array([1]))
+device = torch.device('cuda:0')
+
+# Matplotlib
+import matplotlib.pyplot as plt
+# Numpy
+import numpy as np
+# Pillow
+from PIL import Image
+# Torch
+import torch
+from torch.utils.data import Dataset, DataLoader
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torchvision import transforms
+
+from lung_dataset import Lung_Dataset
+from torch.utils.data import Dataset, DataLoader
+
+train_ld = Lung_Dataset('train', 'three_class')
+
+# train_ld.describe()
+
+train_loader = DataLoader(train_ld, batch_size=512, shuffle=True)
+
+lossGraph = []
+def train_model(model, epochs=50):
+    model.train()
+
+    optim = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+    # maxCoutn = 10
+    # curCount = 0
+    
+    for i in range(epochs):
+        running_loss = 0
+        for batch_idx, (images_data, target_labels) in enumerate(train_loader):
+            images_data = images_data.to(device)
+            target_labels = target_labels.to(device)
+            
+            optim.zero_grad()
+
+            prediction = model(images_data)
+            loss = nn.CrossEntropyLoss()
+            output = loss(prediction, target_labels)
+            output.backward()
+            
+            optim.step()  # gradient descent
+
+            # print('pred:', np.argmax(prediction.detach().numpy(), axis=1))
+            # print('target:', target_labels)
+            # print(output.item())
+            running_loss += output.item()
+
+            # curCount += 1
+            # if curCount > maxCoutn:
+            #     break
+
+        lossGraph.append(running_loss)
+        print(f'Epoch: {i} Loss: {running_loss}')    
 
 
-# def train_model(model):
-#     model.train()
-#     prediction = model(im)
-#     # target = model(im)
-
-#     loss = nn.CrossEntropyLoss()
-#     output = loss(prediction, target)
-#     output.backward()
-#     optim = torch.optim.SGD(model.parameters(), lr=1e-2, momentum=0.9)
-#     optim.step()  # gradient descent
-
-#     print(prediction)
-#     # for batch_idx, (images_data, target_labels) in enumerate(train_loader):
-#     #     predicted_labels = model(images_data)
-#     #     print(predicted_labels)
-#     #     print(target_labels)
-#     #     # Forced stop
-#     #     break
-
-
-# model_three_class = ModelThreeClass()
-# train_model(model_three_class)
+model_three_class = ModelThreeClass()
+model_three_class.to(device)
+train_model(model_three_class)
+# print(torch.cuda.is_available())
