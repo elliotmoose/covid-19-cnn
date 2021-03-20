bc5f933a62dd0927dca17b3b4534d779c62bb1a0
Elliot Koh <appdev.moose@gmail.com>
2021-03-20 22:19:28 +0800

    added confusion matrix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Matplotlib
 import matplotlib.pyplot as plt
+import seaborn as sn
 # Numpy
 import numpy as np
 # Pillow
@@ -16,6 +17,7 @@ from torch.utils.data import Dataset, DataLoader
 
 import model_binary
 import model_three_class
+import model_trainer
 
 
 
@@ -23,52 +25,75 @@ import model_three_class
 # from aug_data_generator import plot_comparison
 # plot_comparison('hist_equal')
 
-# ld_train1 = Lung_Dataset("train", "binary1")
-# ld_val1 = Lung_Dataset("val", "binary1")
-# ld_test1 = Lung_Dataset("test", "binary1")
-
-# ld_train2 = Lung_Dataset("train", "binary2")
-# ld_val2 = Lung_Dataset("val", "binary2")
-# ld_test2 = Lung_Dataset("test", "binary2")
-# print(ld_train1.describe())
 
 
 #====================================================================================
 #=====================         CASCADED BINARY MODEL         ========================
 #====================================================================================
 
-# batch_size = 128
-# n_epochs = 20
-# lr = 0.00001
-# saved_model_path =  "./model/"
+ld_train1 = Lung_Dataset("train", "binary1")
+ld_val1 = Lung_Dataset("val", "binary1")
+ld_test1 = Lung_Dataset("test", "binary1")
+
+ld_train2 = Lung_Dataset("train", "binary2")
+ld_val2 = Lung_Dataset("val", "binary2")
+ld_test2 = Lung_Dataset("test", "binary2")
+print(ld_train1.describe())
+
+batch_size = 128
+n_epochs = 20
+lr = 0.00001
+saved_model_path =  "./model/"
 # device = "cpu"
-# # device = "cuda"
+device = "cuda"
 
-# classifier1 = model_binary.Model_binary_bn()
-# classifier2 = model_binary.Model_binary_bn()
+classifier1 = model_binary.Model_binary_bn()
+classifier2 = model_binary.Model_binary_bn()
 
-# classifier1.to(device)
-# classifier2.to(device)
+classifier1.to(device)
+classifier2.to(device)
 
-# train_loader1 = DataLoader(ld_train1, batch_size=batch_size, shuffle=True)
-# val_loader1 = DataLoader(ld_val1, batch_size=batch_size, shuffle=True)
-# test_loader1 = DataLoader(ld_test1, batch_size=batch_size, shuffle=True)
+train_loader1 = DataLoader(ld_train1, batch_size=batch_size, shuffle=True)
+val_loader1 = DataLoader(ld_val1, batch_size=batch_size, shuffle=True)
+test_loader1 = DataLoader(ld_test1, batch_size=batch_size, shuffle=True)
 
-# train_loader2 = DataLoader(ld_train2, batch_size=batch_size, shuffle=True)
-# val_loader2 = DataLoader(ld_val2, batch_size=batch_size, shuffle=True)
-# test_loader2 = DataLoader(ld_test2, batch_size=batch_size, shuffle=True)
+train_loader2 = DataLoader(ld_train2, batch_size=batch_size, shuffle=True)
+val_loader2 = DataLoader(ld_val2, batch_size=batch_size, shuffle=True)
+test_loader2 = DataLoader(ld_test2, batch_size=batch_size, shuffle=True)
 
 
-# trained_classifier1 = model_binary.train(classifier1, 'bn_class_1', batch_size, n_epochs, lr, train_loader1, val_loader1, saved_model_path, device)
-# model_binary.test_model(trained_classifier1,test_loader1, device)
-# trained_classifier2 = model_binary.train(classifier2, 'bn_class_2', batch_size, n_epochs, lr, train_loader2, val_loader2, saved_model_path, device)
+trained_classifier1 = model_trainer.train(classifier1, 'bn_class_1', batch_size, n_epochs, lr, train_loader1, test_loader1, saved_model_path, device)
+accuracy, confusion_matrix = model_trainer.test(trained_classifier1,test_loader1, device)
+trained_classifier2 = model_trainer.train(classifier2, 'bn_class_2', batch_size, n_epochs, lr, train_loader2, test_loader2, saved_model_path, device)
 
+#show confusion matrix
+sn.set(font_scale=1.4)
+sn.heatmap(confusion_matrix, annot=True, annot_kws={"size": 16})
+plt.show()
 
 #====================================================================================
-#=======================         VALIDATION DISPLAY         =========================
+#========================         THREE CLASSIFIER         ==========================
 #====================================================================================
-model = model_three_class.ModelThreeClassLite()
+batch_size = 512
+n_epochs = 25
+lr = 1e-3
+ld_train3 = Lung_Dataset("train", "three_class")
+ld_val3 = Lung_Dataset("val", "three_class")
+ld_test3 = Lung_Dataset("test", "three_class")
 
+train_loader3 = DataLoader(ld_train3, batch_size=batch_size, shuffle=True)
+val_loader3 = DataLoader(ld_val3, batch_size=batch_size, shuffle=True)
+test_loader3 = DataLoader(ld_test3, batch_size=batch_size, shuffle=True)
+
+three_classifier = model_three_class.ModelThreeClassLite()
+
+trained_three_classifier = model_trainer.train(three_classifier, 'three_class', batch_size, n_epochs, lr, train_loader3, test_loader3, saved_model_path, device)
+accuracy, confusion_matrix = model_trainer.test(trained_three_classifier,test_loader3, device)
+
+#show confusion matrix
+sn.set(font_scale=1.4)
+sn.heatmap(confusion_matrix, annot=True, annot_kws={"size": 16})
+plt.show()
 
 #====================================================================================
 #=======================         VALIDATION DISPLAY         =========================
@@ -81,7 +106,7 @@ fig, axes = plt.subplots(8, 3,figsize=(10,10))
 labels = ["normal", "infected covid", "infected non-covid"]
 for i, (img, label) in enumerate(val_dataloader):
     groundTruth = labels[label]
-    output = model(img)
+    output = three_classifier(img)
     prediction = labels[output.max(dim=1)[1]]
     # print(prediction)
 
